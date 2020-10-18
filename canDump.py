0ab8e98861daa679109a5065082d769f108b2b54
Zain Ahmed <zainahmed1559@gmail.com>
2020-10-18 10:49:57 -0400

    automatically finds CANable usb device

--- canDump.py
+++ canDump.py
@@ -1,15 +1,21 @@
 import can
+from serial.tools import list_ports
 
-def canDump(CANablePort):
-    bus = can.interface.Bus(bustype='slcan', channel=CANablePort, bitrate=250000)
+def selectPort():
+    for port in list_ports.comports():
+        if port.manufacturer == 'Protofusion Labs':
+            print(f'Canable found at {port.device}')
+            return port.device
+
+
+def canDump(CANablePort, bitRate):
+    bus = can.interface.Bus(bustype='slcan', channel=CANablePort, bitrate=bitRate)
     inMsg = can.Message(data=[0, 0, 0, 0, 0, 0, 0, 0], arbitration_id=0, dlc=8, is_extended_id=False)
-
-    print("ID    DLC \tDATA")
 
     while 1:
         for inMsg in bus:
             IDString = '{:02X}'.format(inMsg.arbitration_id)
-            print('\033[96m' + f'0x{IDString}  [{inMsg.dlc}]  ' + '\033[0m', end='')
+            print(f'0x{IDString}\t[{inMsg.dlc}]  ', end='')
 
             for i in range(inMsg.dlc):
                 byteString = '{:02X}'.format(inMsg.data[i])
@@ -17,5 +23,4 @@ def canDump(CANablePort):
 
             print('')
 
-
-canDump('/dev/cu.usbmodem14101')
+canDump(selectPort(), 250000)
